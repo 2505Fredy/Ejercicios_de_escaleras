526c8c3aa6bcb9bd283c9621da7a00acaebf8543
Fredy-123 <71558727+2505Fredy@users.noreply.github.com>
2021-04-08 10:55:33 -0500

    Create Ejercicio1- 3 en raya.cpp
    Definir una matriz de 3x3 caracteres y crear una función para inicializarla con caracteres de '1' hasta el '9'
    El siguiente paso consiste en imprimir dicha matriz en formato de tablero.

---
+++ Ejercicio1- 3 en raya.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+void linea(){
+  for (int i=0; i<11; i++) cout << "-";
+  cout << endl; return;
+}
+
+void tablero(){
+  int valor=1;
+  for (int i=0; i<5; i++){
+    if (i%2==0){ 
+      for (int j=0; j<10; j++){
+        if (j%2==0) cout << " ";
+        else{
+          if (j==1 || j==5 || j==9){
+            cout << valor;
+            if (valor%3==0) cout << endl; valor++;
+          }
+          else cout << "|";
+        }
+      }      
+    }
+    else linea();
+  }
+}
+
+int main(){
+  cout << "Hola mundo" << endl;
+  tablero();
+  return 0;
+}
